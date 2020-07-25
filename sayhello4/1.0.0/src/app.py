5008cff8e659ce1d8073f4f7a4c2b71516c528f8
social-ran <68770158+social-ran@users.noreply.github.com>
2020-07-25 23:55:57 +0800

    Create app.py

---
+++ sayhello4/1.0.0/src/app.py
@@ -0,0 +1,46 @@
+import socket
+import asyncio
+import time
+import random
+import json
+
+from walkoff_app_sdk.app_base import AppBase
+
+class HelloWorld(AppBase):
+    __version__ = "1.0.0"
+    app_name = "hello_world"  # this needs to match "name" in api.yaml
+
+    def __init__(self, redis, logger, console_logger=None):
+
+        super().__init__(redis, logger, console_logger)
+
+    async def hello_world(self):
+        message = f"Hello World from {socket.gethostname()} in workflow {self.current_execution_id}!"
+        #加f表示格式化字符串，可以用花括号！！
+        # This logs to the docker logs
+        self.logger.info(message)
+
+        return message
+
+    async def repeat_back_to_me(self, call):
+        return call
+
+    async def return_plus_one(self, number):
+        return str(number + 1)
+
+    async def pause(self, seconds):
+        time.sleep(seconds)
+        return "Waited %d seconds" % seconds
+
+    async def get_type(self, value):
+        return "Type: %s" % type(value)
+    async def return_plus_two(self, number):
+        return str(number + 2)
+    async def return_plus_three(self, number):
+        return str(number + 3)
+    async def plus_plus(self, number1, number2):
+        return str(number1 + number2)
+
+
+if __name__ == "__main__":
+    asyncio.run(HelloWorld.run(), debug=True)
