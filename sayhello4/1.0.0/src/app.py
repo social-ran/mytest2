2c1699a2426992dfbbd800f1ff597e8b2a6026ed
social-ran <68770158+social-ran@users.noreply.github.com>
2020-07-26 14:58:08 +0800

    Update app.py

--- sayhello4/1.0.0/src/app.py
+++ sayhello4/1.0.0/src/app.py
@@ -36,8 +36,7 @@ class HelloWorld(AppBase):
         return "Type: %s" % type(value)
     async def return_plus_two(self, number):
         return str(number + 2)
-    async def plus_plus(self, number1, number2):
-        return str(number1 + number2)
+
 
 
 if __name__ == "__main__":
